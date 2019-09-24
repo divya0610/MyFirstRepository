415e92c6221ff61d6cd74289e3734d3c0bb5176f
user6 <user6@user6-desktop>
2019-09-24 14:53:18 +0530

    initial commit by Divya

---
+++ SalesToolv2019/src/com/ust/app/Main.java
@@ -0,0 +1,19 @@
+package com.ust.app;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Sales sales = new Sales();
+		System.out.println("Welcome");
+		displayGreeting();
+		sales.display();
+		
+
+	}
+	private static void displayGreeting(){
+		System.out.println("Displaying Sales");
+	}
+
+}
---
+++ SalesToolv2019/src/com/ust/app/Sales.java
@@ -0,0 +1,18 @@
+package com.ust.app;
+
+public class Sales {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	public void display() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+
+		
+	}
+
+}
